c3faed750c36bc17dd0a5e9fe4d43f1802595b4a
= <slotuser@gmail.com>
2020-05-08 19:56:31 +0200

    Created Header, Banner compo. Basic styling. Basic App skeleton. Cleaned vuetify default code.

---
+++ src/components/Banner.vue
@@ -0,0 +1,26 @@
+<template>
+	<v-container fluid>
+		<div class="white--text banner d-flex flex-column justify-center align-center">
+			<h3 class="text-center display-2">HOA FIGHT</h3>
+			<p>LE jeu de platforme-combat du Web !</p>
+			<v-btn class="text-uppercase">jouez gratuitement</v-btn>
+		</div>
+	</v-container>
+</template>
+
+<script>
+	export default {
+		name: 'Banner',
+
+		data: () => ({
+
+		})
+	}
+</script>
+
+<style scoped>
+	.banner {
+		height: 600px;
+		background-color: #484747;
+	}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+	<v-container fluid>
+		<div class="main-wrapper d-flex flex-row">
+			<img alt="LOGO" src=""/>
+			<nav>
+				<ul class="d-flex flex-row justify-space-around">
+					<li v-on:click.prevent v-for="(value, index) in pages" 
+					v-bind:key="index">
+						<a class="text-uppercase black--text" v-bind:href="value.link"> 
+							{{ value.name }}
+						</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	</v-container>
+</template>
+
+<script>
+	export default {
+		name: 'Header',
+
+		data: () => ({
+			pages: [
+				{name: "acceuil", link: ""},
+				{name: "inscription", link: ""},
+				{name: "connexion", link: ""}
+			]
+		})
+	}
+</script>
+
+<style scoped>
+
+	nav ul {
+		list-style-type: none;
+	}
+
+	a {
+		text-decoration: none;
+	}
+
+</style>
+
+
