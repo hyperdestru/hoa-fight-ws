b089954283af1229540aaf4a0fea10134ce7fdf7
= <slotuser@gmail.com>
2020-05-10 13:55:43 +0200

    Wrote basic endpoints

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,23 @@
-console.log("hello, world")
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combine'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+	res.send({
+		message: "hello, home"
+	})
+})
+
+app.get('/status', (req, res) => {
+	res.send({
+		message: "hello, status"
+	})
+})
+
+app.listen(process.env.PORT || 3000)
