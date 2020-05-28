c3ad5b66d87b4c442b52ffb3aa5e0ed9693fc321
= <slotuser@gmail.com>
2020-05-28 19:48:34 +0200

    Making views part 5: stats card of user's dashboard.

--- client/src/components/StatsCard.vue
+++ client/src/components/StatsCard.vue
@@ -1,8 +1,21 @@
 <template>
-	<v-card tile class="card d-flex flex-column align-center">
+	<v-card tile class="card pa-10 d-flex flex-column align-center">
 		<v-card-title class="text-uppercase">
 			Stats
 		</v-card-title>
+		<v-progress-circular
+			size="256"
+			rotate="270"
+			width="32"
+			color="#3F5CD0"
+			value="90"
+		>
+			<ul class="pl-0 d-flex flex-column justify-center align-center">
+				<li class="display-1"> {{ getRatio() }}% </li>
+				<li class="won"> {{ user.games.won }} gagnée(s)</li>
+				<li> {{ user.games.total - user.games.won }} perdue(s)</li>
+			</ul>
+		</v-progress-circular>
 	</v-card>
 </template>
 
@@ -11,10 +24,31 @@
 		name: "StatsCard",
 		
 		data: () => ({
-
+			user: {
+				ratio: null,
+				games: {
+					total: 10,
+					won: 9,
+				}
+			}
 		}),
+
+		methods: {
+			getRatio: function() {
+				return (this.user.games.won*100)/this.user.games.total;
+			}
+		}
 	}
 </script>
 
 <style scoped>
+	ul {
+		list-style-type: none;
+		color: #000;
+		font-weight: bold;
+	}
+
+	ul .won {
+		color: "#3F5CD0";
+	}
 </style>
