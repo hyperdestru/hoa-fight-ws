414ad75d38405c18a3dd79bce1898bee5ab54e8c
= <slotuser@gmail.com>
2020-06-07 13:57:51 +0200

    Delete account form + delete btn overlay (displaying delete account form).

--- client/src/components/DeleteAccountBtn.vue
+++ client/src/components/DeleteAccountBtn.vue
@@ -1,21 +1,49 @@
 <template>
-	<v-btn 
-		@click="deleteAccount"
-		min-width="100%" 
-		small 
-		tile 
-		color="error"
-	>
-		<v-icon small>
-			mdi-delete
-		</v-icon>
-		{{ $t('messages.ctaDeleteAccount') }}
-	</v-btn>
+	<div>
+		<v-btn 
+			@click="overlay = !overlay"
+			min-width="100%" 
+			small 
+			tile 
+			color="error"
+		>
+			<v-icon small>
+				mdi-delete
+			</v-icon>
+			{{ $t('messages.ctaDeleteAccount') }}
+		</v-btn>
+
+		<v-overlay 
+			:value="overlay" 
+			opacity="1"
+			color="primary"
+			class="text-center"
+		>
+			<delete-account-form>
+			</delete-account-form>
+
+			<v-btn @click="overlay = false" icon x-large class="mt-10">
+				<v-icon>
+					mdi-close
+				</v-icon>
+			</v-btn>
+		</v-overlay>
+	</div>
 </template>
 
 <script>
+	import DeleteAccountForm from '@/components/DeleteAccountForm'
+
 	export default {
 		name: "DeleteAccountBtn",
+
+		components: {
+			'delete-account-form': DeleteAccountForm
+		},
+
+		data: () => ({
+			overlay: false
+		}),
 
 		methods: {
 			deleteAccount: function() {
---
+++ client/src/components/DeleteAccountForm.vue
@@ -0,0 +1,34 @@
+<template>
+	<v-form class="pa-10">
+		<div class="mb-10">
+			<h4 class="display-1">
+				{{ $t('messages.tDeleteAccount') }}
+			</h4>
+			<p class="subtitle-1">
+				{{ $t('messages.stDeleteAccountWarning') }}
+			</p>
+		</div>
+
+		<div class="pa-4">
+			<p class="caption mb-0">
+				{{ $t('messages.lEnterEmail') }}
+			</p>
+			<v-text-field :label="$t(label)">
+			</v-text-field>
+		</div>
+
+		<v-btn color="error" min-width="100%">
+			{{ $t('messages.ctaDeleteAccountPlus') }}
+		</v-btn>
+	</v-form>
+</template>
+
+<script>
+	export default {
+		name: 'DeleteAccountForm',
+
+		data: () => ({
+			label: 'messages.lEmail'
+		})
+	}
+</script>
