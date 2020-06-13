41af7920d5d34131f5729036464551d0b1b2f2bb
= <slotuser@gmail.com>
2020-06-13 20:38:39 +0200

    Made header logo component.

---
+++ client/src/components/HeaderLogo.vue
@@ -0,0 +1,15 @@
+<template>
+	<router-link :to="{name: 'home'}">
+		<v-btn icon>
+			<v-icon :x-large="$vuetify.breakpoint.smAndUp">
+				mdi-boxing-glove
+			</v-icon>
+		</v-btn>
+	</router-link>
+</template>
+
+<script>
+	export default {
+		name: 'HeaderLogo'
+	}
+</script>
