98c4180c3fceb12742e7f80517c3116ed0d057b7
= <slotuser@gmail.com>
2020-06-27 16:55:42 +0200

    Got on with create and find User methods.

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -4,21 +4,46 @@ const bcrypt = require('bcrypt');
 const SALT_ROUNDS = 10;
 
 module.exports = {
+	// Faudrait Promisify tout ça
 	async create(params) {
-		bcrypt.hash(params.password, SALT_ROUNDS, function(err, hash) {
-			if (err) {
-				throw err;
+		bcrypt.hash(params.password, SALT_ROUNDS, function(error, hash) {
+			if (error) {
+				throw error;
 			} else {
 				db.connection.query(
 					'INSERT INTO users (email, username, password) VALUES (?, ?, ?)',
 					[params.email, params.username, hash],
-					function(error, rows, fields) {
+					function(error, result, fields) {
 						if (error) {
 							throw error;
 						}
 					}
-				)
+				);
 			}
 		});
+	},
+
+	async find(params) {
+		db.connection.query(
+			'SELECT DISTINCT * FROM users WHERE email = ?',
+			[params.email],
+			function (error, result, fields) {
+				if (error) {
+					throw error;
+				} else {
+					bcrypt.compare(params.password, result.password, function(error, result) {
+						if (error) {
+							throw error;
+						} else {
+							if (result) {
+
+							} else {
+								
+							}
+						}
+					})
+				}
+			}
+		);
 	}
 }
