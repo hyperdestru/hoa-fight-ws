56d54874385665c1060d43318fa81254eb1f87b3
= <slotuser@gmail.com>
2020-07-02 14:38:21 +0200

    Created check token middleware file.

---
+++ server/src/checkToken.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const config = require('./config/config');
+
+module.exports = (req, res, next) => {
+	const token = req.headers['authorization'];
+	const headerPrefix = 'Bearer ';
+
+	if (token.startsWith(headerPrefix)) {
+		token = token.slice(headerPrefix.length, token.length);
+	}
+
+	if (token) {
+		jwt.verify(token, config.authentication.jwtSecret, function(error, decoded) {
+			if (error) {
+				res.status(401).send({
+					error: "Le token n'est pas valide"
+				});
+			} else {
+				req.decodedToken = decoded;
+				next();
+			}
+		});
+	} else {
+		res.status(401).send({
+			error: "Token introuvable"
+		});
+	}
+}
